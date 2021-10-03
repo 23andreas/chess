7cbca94bfb1d344098d9f6f582a3a8c9934487d8
Andreas Sandbæk Skønberg <as@wican.no>
2021-10-03 14:14:51 +0200

    update board component
    - update renderSquare() to work with new square component
    - change for loop variables to x/y to improve code readability
    - add key attributes

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,40 +1,31 @@
 import React from 'react';
 import Square from './Square';
+import { SquareProps } from '../types/SquareProps';
 
-export default class Board extends React.Component {
-  renderSquare(color: string): JSX.Element {
-    return (
-      <Square 
-        color={color}
-      />
-    );
+export default class Board extends React.Component<{
+  squares: { [key: number]: SquareProps };
+}> {
+
+  renderSquare(props: SquareProps): JSX.Element {
+    return <Square {...props}/>;
   }
 
-  renderRow(index: number): JSX.Element {
-    const squares = [];
-    
-    for(let i = 0; i < 8; i++){
-      const squareBgColor = (i+index) % 2 == 0 ? 'white' : 'black';
-      squares.push(this.renderSquare(squareBgColor));
+  renderRow(y: number): JSX.Element {
+    const squares: JSX.Element[] = [];
+
+    for (let x = 1; x <= 8; x++) {
+      squares.push(this.renderSquare(this.props.squares[`${x}${y}`]));
     }
 
-    return (
-      <div className="row">
-        {squares}
-      </div>
-    );
+    return <div className="row" key={y}>{squares}</div>;
   }
 
   render(): JSX.Element {
-    const rows = [];
-    for(let i = 0; i < 8; i++){
-      rows.push(this.renderRow(i));
+    const rows: JSX.Element[] = [];
+    for (let y = 8; y > 0; y--) {
+      rows.push(this.renderRow(y));
     }
 
-    return (
-      <div id="board">
-        {rows}
-      </div>
-    );
+    return <div id="board">{rows}</div>;
   }
 }
